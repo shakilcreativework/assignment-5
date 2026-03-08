2cd73bb9f6c835f49b8286c8d1ef6d6311346520
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-03-08 16:43:38 +0600

    Now, priority badges buttons and colors with all style conditionally changes

--- index.js
+++ index.js
@@ -42,14 +42,14 @@ async function renderIssues() {
     }
 
     filtered.forEach(issue => {
-        console.log(issue);
+        // console.log(issue);
 
         const {id, title, description, labels, priority, author, assignee, createdAt, updatedAt} = issue;
         const createdDate = new Date(createdAt).toLocaleDateString();
         // badge buttons
-        const badges = labels.map(lebel => `
-                <button class="flex gap-1 items-center justify-center py-1 px-2 rounded-full uppercase text-xs text-[#EF4444] font-medium bg-[#EF4444]/50"><img class="w-5" src="./assets/Open-Status.png" alt="">${lebel}</button>
-            `).join('');
+        // const badges = labels.map(lebel => `
+        //         <button class="flex gap-1 items-center justify-center py-1 px-2 rounded-full uppercase text-xs text-[#EF4444] font-medium bg-[#EF4444]/50"><img class="w-5" src="./assets/Open-Status.png" alt="">${lebel}</button>
+        //     `).join('');
 
         // create new card div
         const card = document.createElement("div");
@@ -58,15 +58,14 @@ async function renderIssues() {
         card.innerHTML = `
             <div class="p-5 space-y-5">
                 <div class="flex justify-between items-center">
-                    
-                    <img src="${issue?.status === 'open' ? './assets/Open-Status.png' : './assets/Closed-Status.png'}" alt="">
+                    <i class="text-xl ${issue?.status === 'open' ? 'fa-solid fa-circle-check text-[#00A96E]' : 'fa-regular fa-circle-check text-[#A855F7]'}"></i>
                     <button class="py-1 uppercase px-7 rounded-full font-medium ${priority == 'high' ? 'text-[#EF4444] bg-[#EF4444]/50' : priority == 'medium' ? 'text-[#F59E0B] bg-[#F59E0B]/50' : 'text-[#9CA3AF] bg-[#9CA3AF]/50'}" >${priority}</button>
                 </div>
                 <h2 class="font-semibold text-sm text-[#1F2937] capitalize">${title}</h2>
                 <p class="text-[#64748B] text-xs">${description}</p>
                 <div id="badge" class="flex flex-wrap gap-2">
                     ${labels.map(lebel => `
-                        <button class="flex gap-1 items-center justify-center py-1 px-2 rounded-full uppercase text-xs text-[#EF4444] font-medium bg-[#EF4444]/50"><img class="w-5" src="./assets/Open-Status.png" alt="">${lebel}</button>
+                        <button class="flex gap-1 items-center justify-center py-1 px-2 rounded-full uppercase text-xs font-medium ${lebel == 'bug' ? 'text-[#EF4444] bg-[#EF4444]/50 border border-[#EF4444]/40' : lebel == 'help wanted' ? 'text-[#F59E0B] bg-[#F59E0B]/50 border border-[#F59E0B]/40' : lebel == 'enhancement' ? 'text-[#00A96E] bg-[#00A96E]/50 border border-[#00A96E]/40' : lebel == 'documentation' ? 'text-violet-600 bg-violet-600/50 border border-violet-600/40' : lebel == 'good first issue' ? 'text-orange-600 bg-orange-600/50 border border-orange-600/40' :'text-[#9CA3AF] bg-[#9CA3AF]/50 border border-[#9CA3AF]/40'}"><img class="w-5" src="./assets/Open-Status.png" alt="">${lebel}</button>
                     `).join('')}
                 </div>
             </div>
